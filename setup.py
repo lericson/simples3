9d0d0eb59a790759a5125c4ec977c6b6667dc4f5
lericson <ludvig@lericson.se>
2009-06-14 17:10:14 +0200

    Released 0.3.

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python
 
-from distutils.core import setup, Extension
+from distutils.core import setup
 
 import simples3
 usage = "\nUsage\n-----\n\n" + simples3.__doc__
 long_description = open("README", "U").read() + usage
 
-setup(name="simples3", version="0.2",
+setup(name="simples3", version="0.3",
       url="http://lericson.se/",
       author="Ludvig Ericson", author_email="ludvig@lericson.se",
       description="Simple, quick Amazon AWS S3 interface",
